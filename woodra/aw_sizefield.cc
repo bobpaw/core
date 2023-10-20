b41fbe6e285cd898c99432f575522ffb2ddb4c8d
Aiden Woodruff <woodra@rpi.edu>
2023-10-20 15:13:03 -0400

    Add field into before.vtk and use field in configure instead of configureidentity
    Adds a non-constant field mod 14 as we loop through points
    
    Signed-off-by: Aiden Woodruff <woodra@rpi.edu>

---
+++ woodra/aw_sizefield.cc
@@ -0,0 +1,63 @@
+#include "ma.h"
+#include <apf.h>
+#include <gmi_mesh.h>
+#include <apfMDS.h>
+#include <apfShape.h>
+#include <PCU.h>
+#include <lionPrint.h>
+#ifdef HAVE_SIMMETRIX
+#include <gmi_sim.h>
+#include <SimUtil.h>
+#include <MeshSim.h>
+#include <SimModel.h>
+#endif
+#include <pcu_util.h>
+
+int main(int argc, char** argv)
+{
+  PCU_ALWAYS_ASSERT(argc==3);
+  const char* modelFile = argv[1];
+  const char* meshFile = argv[2];
+  MPI_Init(&argc,&argv);
+  PCU_Comm_Init();
+  lion_set_verbosity(1);
+#ifdef HAVE_SIMMETRIX
+  MS_init();
+  SimModel_start();
+  Sim_readLicenseFile(0);
+  gmi_sim_start();
+  gmi_register_sim();
+#endif
+  gmi_register_mesh();
+  ma::Mesh* m = apf::loadMdsMesh(modelFile,meshFile);
+  m->verify();
+  apf::Field *f = apf::createFieldOn(m, "size", apf::SCALAR);
+  ma::Entity* e;
+  ma::Iterator* it = m->begin(0);
+  int i = 1;
+  while (e = m->iterate(it)) {
+    apf::setScalar(f, e, 0, i % 14 + 1);
+    i++;
+  }
+  m->end(it);
+  apf::writeVtkFiles("before",m);
+  ma::Input* in = ma::configure(m, f);
+  in->shouldRunPreZoltan = true;
+  in->shouldRunMidParma = true;
+  in->shouldRunPostParma = true;
+  in->shouldRefineLayer = true;
+  ma::adapt(in);
+  m->verify();
+  apf::writeVtkFiles("after",m);
+  m->destroyNative();
+  apf::destroyMesh(m);
+#ifdef HAVE_SIMMETRIX
+  gmi_sim_stop();
+  Sim_unregisterAllKeys();
+  SimModel_stop();
+  MS_exit();
+#endif
+  PCU_Comm_Free();
+  MPI_Finalize();
+}
+
